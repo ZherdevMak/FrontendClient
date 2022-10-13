622d193526ef6fbcc0e83a85171c57944b19c4a7
ZherdevMak <Zherdev.mak@gmail.com>
2022-10-13 16:55:54 +0300

    next step

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
-
+import {v1} from "uuid";
 
 function App() {
-  let [users,setUsers] = useState<any[]>([])
-  useEffect(() => {
+  let [users, setUsers] = useState<any[]>([])
+  const getUsers = () => {
     axios.get("http://localhost:7500/users")
       .then(res => setUsers(res.data)
       )
-  },[])
+  }
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  const addUser = () => {
+    axios.post("http://localhost:7500/users")
+      .then(res => {
+        getUsers()
+      })
+  }
+
   return (
-    <div>{users.map(u => <div>{u.name}</div>)}</div>
+    <div>
+      <div>
+        <button onClick={addUser}>Add user</button>
+      </div>
+      <div>{users.map(u => <div key={v1()}>{u.name}</div>)}</div>
+    </div>
   );
 }
 
